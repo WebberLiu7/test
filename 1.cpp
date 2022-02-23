657104c325f0d0d622c049f095f89bfc6b24d652
Liu Bowei <535732098@qq.com>
2022-02-23 21:54:34 +0800

    上传文件
    -------------------

---
+++ 1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=100;
+    for (i = 0; i < 5; i++)
+    {
+        /* code */
+        cout << "C++ know loop" << endl;
+    }
+    cout << "i=" << i << endl;
+}
